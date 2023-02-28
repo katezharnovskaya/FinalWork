44dcd26867272310bc8d2f148414cb81d67bc667
Ekaterina <firegirl-83@mail.ru>
2023-02-28 21:35:54 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arr1 = new string[5] {"final", "HW", "1", "task", "A"};
+string[] arr2 = new string[arr1.Length];
+
+
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arr1, arr2);
+PrintArray(arr2);
